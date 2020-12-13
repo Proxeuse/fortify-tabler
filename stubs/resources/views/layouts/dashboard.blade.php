8157c574ad98566bb275b1df372be72d72cd57be
RoelReijn <34895541+RoelReijn@users.noreply.github.com>
2020-12-13 16:16:54 +0100

    ⚡ add and update other blade files

--- stubs/resources/views/layouts/dashboard.blade.php
+++ stubs/resources/views/layouts/dashboard.blade.php
@@ -33,25 +33,67 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 </head>
 
-<body class="antialiased border-top-wide border-primary d-flex flex-column">
-    <div class="content">
-        <div class="container-xl">
-            <div class="row justify-content-center">
-                <div class="col-lg-10 col-xl-9">
-                    <div class="card card-lg">
-                        <div class="card-body">
-                            <div class="alert alert-warning">
-                                Hi there! Thank you for using the <code><a href="https://github.com/Proxeuse/fortify-tabler">Proxeuse/fortify-tabler</a></code> FortifyUI preset.
-                                We've automatically generated all authentication pages (e.g. login, register, password-reset) but you should manually
-                                configure all other pages. You can change this message by editing <code style="color:inherit">/resources/views/layouts/dashboard.blade.php</code><br>
-                                <small> - © Tabler.io CSS framework. Powered by Proxeuse.</small>
+<body class="antialiased d-flex flex-column">
+    <div class="page">
+        <header class="navbar navbar-expand-md navbar-light d-print-none">
+            <div class="container-xl">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a href="{{ route('home') }}" class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pr-0 pr-md-3">
+                    <img src="{{ asset('/static/logo.svg') }}" height="32" alt="{{ config('app.name', 'Laravel') }}"
+                        class="navbar-brand-image" id="logo">
+                </a>
+                <div class="navbar-nav flex-row order-md-last">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown"
+                            aria-label="Open user menu">
+                            <span class="avatar avatar-sm"
+                                style="background-image: url(@if (Auth::user()->avatar) {{ asset('storage/avatars/'.Auth::user()->avatar) }} @else https://api.proxeuse.com/avatars/api/?name={{ urlencode(Auth::user()->name) }}&color=fff&background={{ substr(md5(Auth::user()->name), 0, 6) }} @endif)"></span>
+                            <div class="d-none d-xl-block ps-2">
+                                <div>{{ Auth::user()->name }}</div>
                             </div>
-                            @yield('content')
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                            <a href="{{ route('profile') }}"
+                                class="dropdown-item">Profile Settings</a>
+                            <div class="dropdown-divider"></div>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button type="submit" class="dropdown-item">
+                                    Logout
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </header>
+        @yield('content')
+        <footer class="footer footer-transparent d-print-none">
+            <div class="container">
+                <div class="row text-center align-items-center flex-row-reverse">
+                    <div class="col-lg-auto ml-lg-auto">
+                        <ul class="list-inline list-inline-dots mb-0">
+                            <li class="list-inline-item"><a href="#"
+                                    class="link-secondary">Help</a></li>
+                            <li class="list-inline-item"><a href="#"
+                                    class="link-secondary">Support</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-lg-auto mt-3 mt-lg-0 me-auto">
+                        <ul class="list-inline list-inline-dots mb-0">
+                            <li class="list-inline-item">
+                                Copyright © 2020
+                                <a href="https://github.com/Proxeuse/fortify-tabler" class="link-secondary">Proxeuse</a>.
+                                All rights reserved.
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 </body>
 
 </html>
---
+++ stubs/resources/views/profile/edit.blade.php
@@ -0,0 +1,160 @@
+@extends('layouts.dashboard')
+<div class="content">
+    <div class="container-xl">
+        <div class="row row-cards">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Update Profile Settings</h3>
+                    </div>
+                    <div class="card-body">
+                        @if (session('success'))
+                        <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+
+                        @if ($errors->any())
+                        <div class="alert alert-danger" role="alert">
+                            {{ $errors->first() }}
+                        </div>
+                        @endif
+
+                        <div class="row mb-3">
+                            <div class="col-2 text-center m-auto">
+                                <span
+                                    style="--tblr-avatar-size: 8rem; background-image: url(@if (Auth::user()->avatar) {{ asset('storage/avatars/'.Auth::user()->avatar) }} @else https://api.proxeuse.com/avatars/api/?name={{ urlencode(Auth::user()->name) }}&color=fff&background={{ substr(md5(Auth::user()->name), 0, 6) }}&size=500 @endif)"
+                                    class="avatar avatar-xl"></span>
+                            </div>
+                            <div class="col-10">
+                                <form enctype="multipart/form-data" action="{{ route('profile.avatar') }}"
+                                    method="POST">
+                                    @csrf
+                                    <div class="form-group mb-3">
+                                        <div class="form-label">Upload your own Profile Picture</div>
+                                        <input type="file" name="avatar" class="form-control"
+                                            accept=".png,.jpg,.jpeg,.gif,.webp">
+                                        <small class="form-hint">We recommend uploading a image with a min width of
+                                            200px and a max width of 1000px. Only png, jpg, gif and webp files are
+                                            allowed.</small>
+                                    </div>
+                                    <input type="submit" class="btn btn-primary">
+                                    <button class="btn btn-danger"
+                                        onclick="event.preventDefault(); $('#deleteAvatarForm').submit()">Delete Profile
+                                        Picture</button>
+
+                                </form>
+                            </div>
+                            <form action="{{ route('profile.deleteavatar') }}" method="post" id="deleteAvatarForm">
+                                @csrf
+                                @method('DELETE')
+                            </form>
+
+                        </div>
+
+                        <hr>
+
+                        <form method="POST" action="{{ route('user-profile-information.update') }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="mb-3">
+                                <label class="form-label">Full Name</label>
+                                <input type="text" name="name" class="form-control"
+                                    value="{{ old('name') ?? auth()->user()->name }}" required autofocus
+                                    autocomplete="name" />
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">{{ __('Email') }}</label>
+                                <input type="email" name="email" class="form-control"
+                                    value="{{ old('email') ?? auth()->user()->email }}" required autofocus />
+                            </div>
+
+                            <div>
+                                <button type="submit" class="btn btn-primary">
+                                    Update Profile
+                                </button>
+                            </div>
+                        </form>
+
+                        <hr>
+
+                        <div class="table-responsive">
+                            <table class="table table-vcenter datatable">
+                                <thead>
+                                    <tr>
+                                        <th>User Agent</th>
+                                        <th>IP Address</th>
+                                        <th>Last Activity</th>
+                                        <th class="w-1"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($devices as $device)
+                                    <tr>
+                                        <td>{{ $device->user_agent }}</td>
+                                        <td>
+                                            {{ $device->ip_address }}
+                                        </td>
+                                        <td>
+                                            {{ Carbon\Carbon::createFromTimestamp($device->last_activity)->locale(str_replace('_', '-', app()->getLocale()))->diffForHumans() }}
+                                        </td>
+                                        <td>
+                                            @if(\Session::getId() == $device->id)
+                                            <button disabled="disabled" class="btn btn-primary">Current Device</button>
+                                            @else
+                                            <form action="{{ route('profile.deletedevice', $device->id) }}"
+                                                method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <input type="submit" class="btn btn-danger" value="Remove" />
+                                            </form>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <hr>
+
+                        <form method="POST" action="{{ route('user-password.update') }}">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="mb-3">
+                                <label class="form-label">{{ __('dashboard.profile.update.currentpassword') }}</label>
+                                <input type="password" name="current_password" class="form-control" required
+                                    autocomplete="current-password" />
+                            </div>
+
+                            <div class="mb-3">
+                                <label class="form-label">{{ __('dashboard.profile.update.newpassword') }}</label>
+                                <input type="password" name="password" class="form-control" required
+                                    autocomplete="new-password" />
+                            </div>
+
+                            <div class="mb-3">
+                                <label>{{ __('dashboard.profile.update.currentpassword') }}</label>
+                                <input type="password" name="password_confirmation" class="form-control" required
+                                    autocomplete="new-password" />
+                            </div>
+
+                            <div>
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Save') }}
+                                </button>
+                            </div>
+                        </form>
+
+                        <hr>
+
+                        @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::twoFactorAuthentication()))
+                        @include('profile.two-factor-authentication-form')
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
