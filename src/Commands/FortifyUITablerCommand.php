72201b07f13c23b72fa37ec16bfc1e05d6945772
RoelReijn <34895541+RoelReijn@users.noreply.github.com>
2020-12-13 16:06:58 +0100

    ⚡ Update FortifyUITabler Command and the ServiceProvider
    Make files ready for the latest release by adding an installer to the fortify-ui:tabler command. In addition, save the ServiceProvider for additional migrations, controllers, languages, etc.

--- src/Commands/FortifyUITablerCommand.php
+++ src/Commands/FortifyUITablerCommand.php
@@ -12,13 +12,41 @@ class FortifyUITablerCommand extends Command
 
     public function handle()
     {
-        $this->publishAssets();
+        // confirm the installation
+        if ($this->confirm('Do you wish to continue? Please only continue on a fresh Laravel installation since multiple files are overwritten by the installer.', false)) {
+            // install fortifyUI
+            Artisan::call('fortify-ui:install');
+            $this->info('FortifyUI has been installed. Proceeding to install Tabler.io.');
+            
+            // publish the assets, routes, controllers, etc.
+            $this->publishAssets();
 
-        $this->comment('The Tabler.io Framework is now installed.');
+            // request information on session driver
+            $this->changeSessionDriver();
+
+            // print success message
+            $this->info('The Tabler.io Framework is now installed.');
+            $this->newLine();
+            $this->line('Please run \'php artisan migrate\' before continuing.');    
+        } else {
+            // print abort message
+            $this->error('Installation is aborted');
+        }
     }
 
     protected function publishAssets()
     {
         $this->callSilent('vendor:publish', ['--tag' => 'fortify-ui-tabler-resources', '--force' => true]);
+    }
+
+    protected function changeSessionDriver(){
+        if($this->confirm('This package only works out of the box if you have set your session driver to \'database\', have you already changed it?', true)) {
+            $this->line('Great! We\'ll create a new migration for the table right now.');
+            Artisan::call('php artisan session:table');
+            $this->newLine();
+        } else {
+            $this->line('No problem! Please make sure however to change it manually according to the documentation at https://github.com/proxeuse/fortify-tabler/.');
+            $this->newLine();
+        }
     }
 }
--- src/FortifyUITablerServiceProvider.php
+++ src/FortifyUITablerServiceProvider.php
@@ -10,10 +10,19 @@ class FortifyUITablerServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
+            // Load translation files
+            $this->loadTranslationsFrom(__DIR__.'/../stubs/resources/lang', 'tabler');
+            // Load Routes
+            $this->loadRoutesFrom(__DIR__.'/../stubs/routes/web.php');
+            // Load Migrations
+            $this->loadMigrationsFrom(__DIR__.'/../stubs/database/migrations');
+
+            // Publis files
             $this->publishes([
                 __DIR__ . '/../stubs/resources/views' => base_path('resources/views'),
                 __DIR__ . '/../stubs/public' => base_path('public'),
-                __DIR__ . '/../stubs/lang' => base_path('resources/lang'),
+                __DIR__ . '/../stubs/resources/lang' => base_path('resources/lang/vendor/tabler'),
+                __DIR__ . '/../stubs/app/Http/Controllers' => base_path('app/Http/Controllers'),
             ], 'fortify-ui-tabler-resources');
 
             $this->commands([
