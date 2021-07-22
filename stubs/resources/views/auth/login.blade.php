e52af08aafae026793e4e3d0c6cd39684bc79d2d
Syahril Zulkefli <syahzul@gmail.com>
2021-07-22 14:33:28 +0800

    Fix svg icon for password input

--- stubs/resources/views/auth/login.blade.php
+++ stubs/resources/views/auth/login.blade.php
@@ -27,14 +27,14 @@
                     placeholder="{{ __('auth.placeholder.password') }}" value="{{ old('password') }}" required
                     tabindex="2" />
                 <span class="input-group-text">
-                    <a href="#" class="link-secondary" title="Show password" data-toggle="tooltip"><svg
+                    <a href="#" class="link-secondary" title="Show password" data-toggle="tooltip">
+                        <svg
                             xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
                             stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                             stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <circle cx="12" y="12" r="2" />
-                            <pathc
-                                d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7" />
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="2"></circle>
+                            <path d="M22 12c-2.667 4.667 -6 7 -10 7s-7.333 -2.333 -10 -7c2.667 -4.667 6 -7 10 -7s7.333 2.333 10 7"></path>
                         </svg>
                     </a>
                 </span>
